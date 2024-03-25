a1ca94688f4831d78a761ff9c7138fa7f0ed8ecf
Galevi1998 <135505755+Galevi1998@users.noreply.github.com>
2024-03-25 17:57:45 +0200

    Second Commit

---
+++ Tile.java
@@ -0,0 +1,12 @@
+package test;
+
+
+public class Tile {
+    final char letter;
+    final int score;
+
+    public Tile (char L , int V){
+        this.Value = V;
+        this.score = L;
+    }
+}
